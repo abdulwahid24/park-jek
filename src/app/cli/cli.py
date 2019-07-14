362f1dfe42616cd5b8b18f22029ad822fb7f9065
Abdul Wahid <abdulwahid24@gmail.com>
2019-07-14 17:01:40 +0530

    Implemented a welcome note for the CLI console.

--- src/app/cli/cli.py
+++ src/app/cli/cli.py
@@ -1,6 +1,9 @@
 import sys
+import os
+from app import BASE_DIR
 from app.core import Singleton
 
+CLI_BASE_PATH = os.path.join(BASE_DIR, os.path.dirname(__file__))
 
 class CLIConsole(Singleton):
 
@@ -10,15 +13,19 @@ class CLIConsole(Singleton):
 		self.config = config
 
 	def __enter__(self, *args):
-		print("Welcome")
+		with open(os.path.join(CLI_BASE_PATH, 'welcome.txt'), 'r') as welcome_message_file:
+			print(welcome_message_file.read())
 		return self
 
 	def __exit__(self, *args, **kwargs):
-		print("Good Bye")
+		print("\nGood Bye")
 
 	def run(self):
 		while True:
-			self.std_input = input("park-a-lot> ")
-			if self.std_input == 'exit':
+			try:
+				self.std_input = input("park-a-lot> ")
+				if self.std_input == 'exit':
+					sys.exit()
+			except (KeyboardInterrupt, EOFError):
 				sys.exit()
 
