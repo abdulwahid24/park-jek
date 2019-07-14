4efb15177c03198ca402e2d96839060fe832723d
Abdul Wahid <abdulwahid24@gmail.com>
2019-07-14 15:46:54 +0530

    Reconfigured singleton pattern

--- src/app/core/base.py
+++ src/app/core/base.py
@@ -1,9 +1,10 @@
 
 
 class Singleton:
-	__instance = None
 
 	def __new__(cls, *args, **kwargs):
-		if Singleton.__instance is None:
-			Singleton.__instance = object.__new__(cls)
-		return Singleton.__instance
+		self = "__self__"
+		if not hasattr(cls, self):
+			instance = object.__new__(cls)
+			setattr(cls, self, instance)
+		return getattr(cls, self)
