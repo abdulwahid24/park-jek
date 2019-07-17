00ed19a18dd44da7263660b9d2c81c0cc4d8e351
Abdul Wahid <abdulwahid24@gmail.com>
2019-07-17 09:47:14 +0530

    Added test_create_parking_lot to test create_parking_lot command.

--- src/app/tests/test_parking_lot_commands.py
+++ src/app/tests/test_parking_lot_commands.py
@@ -1,13 +1,18 @@
 import unittest
 import os
-from src.app.tests import TEST_CONFIG_FILE
-from src.app.config.config import get_config
+from app.tests import TEST_CONFIG_FILE
+from app.config.config import get_config
+from app.cli.commands import ParkingLotCommand
 
 
 class TestParkingLotCommands(unittest.TestCase):
     def setUp(self):
         self.AppConfig = get_config(
             config_file=TEST_CONFIG_FILE, execution_level='test')
+        self.parking_lot_command = ParkingLotCommand()
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_create_parking_lot(self):
+        expected_result = "Created a parking lot with 2 slots"
+        actual_result = self.parking_lot_command.execute(
+            'create_parking_lot', 2)
+        self.assertEqual(expected_result, actual_result)
